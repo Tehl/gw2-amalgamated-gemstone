de67218962cc5e5a48fc485e06f1885fe00afe78
Tehl <mat_howe@hotmail.com>
2017-08-08 19:35:46 +0100

    Add boilerplate
    Added React
    Added kube css framework
    Update webpack for less css

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,36 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devServer: {
-        contentBase: './src/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.less$/,
-                loader: [
-                    'style-loader',
-                    'css-loader',
-                    { loader: 'css-loader', options: { importLoaders: 1 } },
-                    'less-loader'
-                ]
-            }
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: "./src/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { importLoaders: 1 } }
         ]
-    }
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "less-loader"
+        ]
+      }
+    ]
+  }
 };
